e7027f7ab7be85f7735dd152dc9201173f71ca77
Juan A. Ruz <juanantonioruz@gmail.com>
2021-02-02 11:58:20 +0100

    Improve view

--- frontend/src/main/hortinvest/ui/impacts.cljs
+++ frontend/src/main/hortinvest/ui/impacts.cljs
@@ -1,6 +1,7 @@
 (ns hortinvest.ui.impacts
   (:require
    [hortinvest.ui.impacts-data :as data]
+   [clojure.string :refer [replace trim]]
    [syn-antd.col :refer [col]]
    [syn-antd.progress :refer [progress]]
    [syn-antd.row :refer [row]]))
@@ -36,44 +37,49 @@
      )
    (:periods i)))
 
+(defn nan [x]
+  (if (js/isNaN x) 0 x))
+
+(defn to-int [x]
+  (. js/Number parseInt x))
+
+(defn period-value [v]
+  (if (and v (not= v ""))
+    (nan (to-int (trim (replace v #"\%" ""))))
+    0))
+
+(defn periods [r i]
+  (let [periods (:periods i)]
+    (reduce (fn [c p]
+              (let [target (period-value (:target_value p))
+                    actual (period-value (:actual_value p))
+                    percent (nan (to-int (* (/ actual target) 100)))]
+               (into c [[col {:span 4} [:div
+                                        [:div (str (:period_start p) " - " (:period_end p))]
+                                        [:div (str (period-value (:actual_value p)) " / " (period-value (:target_value p)))]
+                                        [progress {:percent percent :size "small"}]]]])))
+            r periods )))
+
 (defn impact-indicators [impact]
   (map-indexed
    (fn [item-id i]
      [:div {:key (str "indicator-div-" item-id)}
-      [row {:gutter 20 :key (str "indicator-" item-id)}
-       [col {:span 8}
-        [:p (:title i)]]
-       [col {:span 4}
-        [progress {:percent 0
-                   :size "small"}]]
-       [col {:span 4}
-        [progress {:percent 30
-                   :size "small"}]]
-       [col {:span 4}
-        [progress {:percent 70
-                   :size "small"}]]
-       [col {:span 4}
-        [progress {:percent 100
-                   :size "small"}]]]
-      (indicator-periods i)
-      ]
-     #_[:li {:name (str "indicator-" item-id) :key (str "indicator-" item-id)}
-      [:h4 {:class (project-type p)} (:title i)]
-      #_(indicator-periods i)
-      ])
+      (periods [row {:gutter 20 :key (str "indicator-" item-id)}
+                [col {:span 8} [:p (:title i)]]]
+               i)])
    (:indicators impact)))
 
-(defn show-impacts [impacts]
-  (map-indexed
-   (fn [item-id impact]
-     [:div {:key (str (str "impact-div-" item-id))}
-      [row
-       [col {:span 24 :key (str "impact-" item-id)}
-        [:h3 (:title impact)]]]
-      (impact-indicators impact)
-      (when (:outputs impact)
-        (show-impacts (:outputs impact)))])
-   impacts))
-
-(defn impacts []
-  [:div (show-impacts @data/db)])
+(defn impacts
+  ([]
+   [:div (impacts @data/db)])
+  ([topics]
+   (map-indexed
+    (fn [item-id impact]
+      [:div {:key (str (str "impact-div-" item-id))}
+       [row
+        [col {:span 24 :key (str "impact-" item-id)}
+         [:h3 (:title impact)]]]
+       (impact-indicators impact)
+       (when (:outputs impact)
+         (impacts (:outputs impact)))])
+    topics)))
