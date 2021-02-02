c77f84c38af9bce0bbf7915b284e91fc41c27fca
Daniel Sunnerek <daniel@akvo.org>
2021-02-02 16:00:02 +0100

    Updated dashboards

--- frontend/src/main/hortinvest/app.cljs
+++ frontend/src/main/hortinvest/app.cljs
@@ -7,12 +7,20 @@
 (def dashboard-config
   [{:id "60117663-3feb-4b48-a44a-a02b6961a9bc"
     :title "Fruit tree & seed distribution"
-    :height "4970"
+    :height "2570"
     :src "https://hortinvest.akvolumen.org/s/2X0tZojm71g"}
    {:id "600eec0a-dd6e-46ae-a8d4-53b5fc3ad1a4"
     :title "Business cases"
-    :height "6620"
-    :src "https://hortinvest.akvolumen.org/s/534M4nFv6TE"}])
+    :height "5450"
+    :src "https://hortinvest.akvolumen.org/s/534M4nFv6TE"}
+   {:id "6017f275-874a-4c1e-af0b-98a39b84f019"
+    :title "Cooperatives"
+    :height "2330"
+    :src "https://hortinvest.akvolumen.org/s/aI2UtX3Mhio"}
+   {:id "6017f15f-1cb1-497c-adfc-2b25394257d4"
+    :title "Demo sites"
+    :height "1490"
+    :src "https://hortinvest.akvolumen.org/s/7jbe6xb2l1E"}])
 
 (defonce app-state
   (r/atom {:main-menu-selection "projects"
