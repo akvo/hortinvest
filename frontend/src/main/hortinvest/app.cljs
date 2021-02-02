021c0f900819b8f1d3ade834088d59223a879de0
Daniel Sunnerek <daniel@akvo.org>
2021-02-02 21:29:02 +0100

    Added Events and Production and data collection dashboards

--- frontend/src/main/hortinvest/app.cljs
+++ frontend/src/main/hortinvest/app.cljs
@@ -20,7 +20,15 @@
    {:id "6017f15f-1cb1-497c-adfc-2b25394257d4"
     :title "Demo sites"
     :height "1490"
-    :src "https://hortinvest.akvolumen.org/s/7jbe6xb2l1E"}])
+    :src "https://hortinvest.akvolumen.org/s/7jbe6xb2l1E"}
+   {:id "6019998f-ee59-481c-9296-487767e094c5"
+    :title "Production and Data collection"
+    :height "1000"
+    :src "https://hortinvest.akvolumen.org/s/JEBKlzrgdag"}
+   {:id "601998f0-061f-4399-b0cb-f57c7e1dab96"
+    :title "Events"
+    :height "1000"
+    :src "https://hortinvest.akvolumen.org/s/XjKn6k2yRmM"}])
 
 (defonce app-state
   (r/atom {:main-menu-selection "projects"
