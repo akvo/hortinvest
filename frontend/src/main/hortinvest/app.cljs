a413699e5d8decfd67096fae94c7a5a939d9999a
Juan A. Ruz <juanantonioruz@gmail.com>
2021-02-10 19:23:30 +0100

    [#15] Connect to sentry
    Closes #15

--- frontend/src/main/hortinvest/app.cljs
+++ frontend/src/main/hortinvest/app.cljs
@@ -3,7 +3,11 @@
    [hortinvest.config :as config]
    [hortinvest.ui :as ui]
    [reagent.core :as r]
+   ["@sentry/tracing" :as tracing :refer [Integrations]]
+   ["@sentry/browser" :as Sentry]
+
    [reagent.dom :as rdom]))
+
 
 
 (defonce app-state
@@ -11,5 +15,8 @@
            :projects {:config config/dashboard-config}}))
 
 (defn init []
+  (Sentry/init (clj->js {:dsn "https://74a424e902fa437ab1a424ac2391ce07@o65834.ingest.sentry.io/5632052"
+                         :integrations [(new (. Integrations -BrowserTracing))]
+                         :tracesSampleRate 1.0}))
   (rdom/render [ui/root app-state]
                (js/document.getElementById "app")))
