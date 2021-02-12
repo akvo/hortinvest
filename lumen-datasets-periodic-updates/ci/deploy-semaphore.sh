2992c96399a40d1dc6d4d1dbec17da1955a81ab7
Juan A. Ruz <juanantonioruz@gmail.com>
2021-02-12 17:37:07 +0100

    [#16] Try deployment

--- lumen-datasets-periodic-updates/ci/deploy-semaphore.sh
+++ lumen-datasets-periodic-updates/ci/deploy-semaphore.sh
@@ -8,13 +8,13 @@ function log {
 
 export PROJECT_NAME=akvo-lumen
 
-if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
-    exit 0
-fi
+#if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+#    exit 0
+#fi
 
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-    exit 0
-fi
+#if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+#    exit 0
+#fi
 
 log Authentication with gcloud and kubectl
 gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json --project "${PROJECT_NAME}"
@@ -23,19 +23,19 @@ gcloud config set compute/zone europe-west1-d
 gcloud config set container/use_client_certificate False
 
 ## TODO!!! Change to prod! Decide if we publish to test or not
-if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-    log Environment is production
-    log Project not deployed to production cluster. Exiting now.
-    exit 0
-else
+#if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+#    log Environment is production
+#    log Project not deployed to production cluster. Exiting now.
+#    exit 0
+#else
     log Environement is test
     gcloud container clusters get-credentials test
-fi
+#fi
 
 log Pushing images
 gcloud auth configure-docker
-docker push "eu.gcr.io/${PROJECT_NAME}/hortinvest-lumen-updates:${TRAVIS_COMMIT}"
-
-sed -e "s/\${TRAVIS_COMMIT}/$TRAVIS_COMMIT/g" ci/k8s/cronjob.yaml.template > cronjob.yaml.donotcommit
+docker push "eu.gcr.io/${PROJECT_NAME}/hortinvest-lumen-updates:${CI_COMMIT}"
+cd lumen-datasets-periodic-updates
+sed -e "s/\${CI_COMMIT}/$CI_COMMIT/g" ci/k8s/cronjob.yaml.template > cronjob.yaml.donotcommit
 
 kubectl apply -f cronjob.yaml.donotcommit
