d2ef6bdca798b002297c54f7b7cdf76dcdecc462
Daniel Sunnerek <daniel@akvo.org>
2021-02-15 18:29:13 +0100

    [#14] All dashboards

--- frontend/src/main/hortinvest/config.cljs
+++ frontend/src/main/hortinvest/config.cljs
@@ -1,36 +1,50 @@
 (ns hortinvest.config)
 
 (def dashboard-config
-  [{:id "1"
-    :src-id "60117663-3feb-4b48-a44a-a02b6961a9bc"
-    :title "Fruit tree & seed distribution"
-    :height "2570"
-    :src "https://hortinvest.akvolumen.org/s/2X0tZojm71g"}
-   {:id "2"
-    :src-id "600eec0a-dd6e-46ae-a8d4-53b5fc3ad1a4"
-    :title "Business cases"
-    :height "5450"
-    :src "https://hortinvest.akvolumen.org/s/534M4nFv6TE"}
-   {:id "3"
-    :src-id "6017f275-874a-4c1e-af0b-98a39b84f019"
-    :title "Cooperatives"
-    :height "2330"
-    :src "https://hortinvest.akvolumen.org/s/aI2UtX3Mhio"}
-   {:id "4"
-    :src-id "6017f15f-1cb1-497c-adfc-2b25394257d4"
-    :title "Demo sites"
-    :height "1490"
-    :src "https://hortinvest.akvolumen.org/s/7jbe6xb2l1E"}
-   {:id "5"
-    :src-id "6019998f-ee59-481c-9296-487767e094c5"
-    :title "Production and Data collection"
-    :height "1000"
-    :src "https://hortinvest.akvolumen.org/s/JEBKlzrgdag"}
-   {:id "6"
-    :src-id "601998f0-061f-4399-b0cb-f57c7e1dab96"
-    :title "Events"
-    :height "1000"
-    :src "https://hortinvest.akvolumen.org/s/XjKn6k2yRmM"}])
+  [{:height "1000"
+    :id "1"
+    :src "https://hortinvest.akvolumen.org/s/7jbe6xb2l1E"
+    :title "Demo Site"}
+   {:height "1000"
+    :id "2"
+    :src "https://hortinvest.akvolumen.org/s/F6UPzdpVkHs"
+    :title "Farmer group"}
+   {:height "1000"
+    :id "3"
+    :src "https://hortinvest.akvolumen.org/s/aI2UtX3Mhio"
+    :title "Cooperatives"}
+   {:height "1000"
+    :id "4"
+    :src "https://hortinvest.akvolumen.org/s/yBs6eu3KLzA"
+    :title "SMEs"}
+   {:height "1000"
+    :id "5"
+    :src "https://hortinvest.akvolumen.org/s/534M4nFv6TE"
+    :title "Business cases"}
+   {:height "1000"
+    :id "6"
+    :src "https://hortinvest.akvolumen.org/s/2X0tZojm71g"
+    :title "Fruit tree registration"}
+   {:height "1000"
+    :id "7"
+    :src "https://hortinvest.akvolumen.org/s/eyq63eE0SXM"
+    :title "Fruit tree monitoring"}
+   {:height "1000"
+    :id "8"
+    :src "https://hortinvest.akvolumen.org/s/JEBKlzrgdag"
+    :title "Production"}
+   {:height "1000"
+    :id "9"
+    :src "https://hortinvest.akvolumen.org/s/ZIXUeTwIBYo"
+    :title "Sales"}
+   {:height "1000"
+    :id "10"
+    :src "https://hortinvest.akvolumen.org/s/d6pZ0NRyMnU"
+    :title "Meetings"}
+   {:height "1000"
+    :id "11"
+    :src "https://hortinvest.akvolumen.org/s/NRuVFl77ijM"
+    :title "Training"}])
 
 (def outcomes
   [{:title "Outcome 1"
